9c58c096f5004a7ba87787d84fe65ca715290ba8
abhishekgedelacode <gabhishekji143@gmail.com>
2021-05-07 10:24:56 +0530

    the soldier and banana prog is soved after getting two erorrs

---
+++ solBanana.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int k, n, w, i, sum = 0;
+    scanf("%d%d%d", &k, &n, &w);
+    for (i = 1; i <= w; i++)
+    {
+        sum = sum + i * k;
+    }
+    if (sum > n)
+        printf("%d\n", sum - n);
+    else
+        printf("0");
+    return 0;
+}
